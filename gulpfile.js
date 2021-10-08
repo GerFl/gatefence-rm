7a3fe1aade643082f2c4aee8022d02c6c507d22a
Gerardo <flores20_03@outlook.com>
2021-10-08 13:08:12 -0500

    Setup

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { src, dest, watch } = require('gulp');
+const gulpSass = require('gulp-sass');
+const dartSass = require('dart-sass');
+const sass = gulpSass(dartSass);
+
+function compilarSass() {
+    return src('./src/scss/app.scss')
+        .pipe(sass())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(dest('./dist/css'));
+}
+
+function uatu() {
+    watch('./src/scss/*.scss', compilarSass);
+}
+
+exports.compilarSass = compilarSass;
+exports.uatu = uatu;
